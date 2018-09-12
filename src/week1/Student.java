508b47871de5e5ec8d1e88cd951a8f230995592a
xuvika3110 <duckhua861999@gmail.com>
2018-09-12 14:38:09 +0700

    123jkl

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,21 +1,26 @@
 package week1;
 
 public class Student {
+    private String name,id,group,email;
 
     // TODO: khai báo các thuộc tính cho Student
-
+    public String getName(){return name;}   public void setName(){this.name=name;}
+    public String getId(){return id;}       public void setId(){this.id=id;}
+    public String getGroup(){return group;}   public void setName(){this.group=group;}
+    public String getEmail(){return email;}   public void setName(){this.email=email;}
     // TODO: khai báo các phương thức getter, setter cho Student
 
     /**
      * Constructor 1
      */
     Student() {
-        // TODO:
+
+        // TODO Auto-generated constructor stub
     }
 
     /**
      * Constructor 2
-     * @param n
+     * @param  n
      * @param sid
      * @param em
      */
