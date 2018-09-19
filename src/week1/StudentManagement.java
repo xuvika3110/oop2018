7bb1cd8530f7330699c2c1e8d23e12bc5629a520
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-09-19 10:05:04 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,12 +1,13 @@
 package week1;
 
 public class StudentManagement {
+    Student students[] = new Student[100];
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
 
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return s1.getGroup()==s2.getGroup();
     }
 
     void studentsByGroup() {
