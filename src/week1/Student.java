d48275e562ff8621f4bdf4bfb84459dd7cbc9262
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-09-19 10:01:01 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -4,16 +4,20 @@ public class Student {
     private String name,id,group,email;
 
     // TODO: khai báo các thuộc tính cho Student
-    public String getName(){return name;}   public void setName(){this.name=name;}
-    public String getId(){return id;}       public void setId(){this.id=id;}
-    public String getGroup(){return group;}   public void setName(){this.group=group;}
-    public String getEmail(){return email;}   public void setName(){this.email=email;}
+    public String getName(){return name;}   public void setName(String name){this.name=name;}
+    public String getId(){return id;}       public void setId(String id){this.id=id;}
+    public String getGroup(){return group;}   public void setName(String group){this.group=group;}
+    public String getEmail(){return email;}   public void setName(String email){this.email=email;}
     // TODO: khai báo các phương thức getter, setter cho Student
 
     /**
      * Constructor 1
      */
     Student() {
+         this.name = "Student";
+        this.id = "111";
+        this.email = "uet@vnu.edu.vn";
+        this.group = "INT22041";
 
         // TODO Auto-generated constructor stub
     }
@@ -26,6 +30,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name = n;
+        this.id = sid;
+        this.email = em;
+        this.group = "INT 22041";
     }
 
     /**
@@ -33,10 +41,14 @@ public class Student {
      * @param s
      */
     Student(Student s) {
+        this.name = s.name;
+        this.id = s.id;
+        this.email = s.email;
         // TODO:
     }
 
     String getInfo() {
+         return this.getName() + "\n" + this.getId() + "\n" + this.getGroup() + "\n" + this.getEmail();
         // TODO:
         return null; // xóa dòng này sau khi cài đặt
     }
