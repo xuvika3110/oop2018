1d4c1e9dda4e4bc655c7bb68bbfefadd0e2b7f41
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-09-21 11:28:49 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -9,6 +9,24 @@ public class Task1 {
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 1;
+ 
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            for (int i = 2; i < n; i++) {
+                f0 = f1;
+                f1 = fn;
+                fn = f0 + f1;
+            }
+        }
+        return fn;
+    }
+
         return 0;
     }
 }
