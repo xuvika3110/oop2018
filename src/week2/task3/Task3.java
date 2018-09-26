ffe38f51ea705ce9a42fe81e7bb7c68093658c7c
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-09-26 12:44:34 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -2,5 +2,12 @@ package week2.task3;
 
 public class Task3 {
 }
+public class Student {
+    private String name,id,group,email;
+    public String getName(){return name;}   public void setName(String name){this.name=name;}
+    public String getId(){return id;}       public void setId(String id){this.id=id;}
+    public String getGroup(){return group;}   public void setGroup(String group){this.group=group;}
+    public String getEmail(){return email;}   public void setEmail(String email){this.email=email;}
+  
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
