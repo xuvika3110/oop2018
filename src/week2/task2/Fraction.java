a4f5e2f0949b80c6757147e1f876868c539724f9
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-09-26 13:40:10 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -5,6 +5,12 @@ public class Fraction {
     // TODO: khai báo các thuộc tính
     int tu;
     int mau;
+    public static int gcd(int a, int b) {
+        // TODO: Tính ước chung lớn nhất của 2 số a, b
+        if(b==0) return a;
+        return gcd(b;a%b);
+        
+    }
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
@@ -16,16 +22,22 @@ public class Fraction {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
         int ts = this.tu * other.mau + other.tu * this.mau;
         int ms = this.mau * other.mau;
+        int u = gcd(ts, ms);
+        ts = ts/u;
+        ms = mau/u;
         Fraction phanSoTong = new Fraction(ts, ms);
-        return Fraction(ts, ms);
+        return Fraction();
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
         int ts = this.tu * other.mau - other.tu * this.mau;
         int ms = this.mau * other.mau;
+        int u = gcd(ts, ms);
+        ts = ts/u;
+        ms = mau/u;
         Fraction phanSoHieu = new Fraction(ts, ms);
-        return Fraction(ts, ms);
+        return Fraction();
         
     }
 
@@ -33,8 +45,11 @@ public class Fraction {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
         int ts = this.tu * other.tu;
         int ms = this.mau * other.mau;
+        int u = gcd(ts, ms);
+        ts = ts/u;
+        ms = mau/u;
         Fraction phanSoTich = new Fraction(ts, ms);
-        return Fraction(ts, ms);
+        return Fraction();
         
     }
 
@@ -42,8 +57,11 @@ public class Fraction {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
         int ts = this.tu * other.mau;
         int ms = this.mau * other.tu;
+        int u = gcd(ts, ms);
+        ts = ts/u;
+        ms = mau/u;
         Fraction phanSoThuong = new Fraction(ts, ms);
-        return Fraction(ts, ms);
+        return Fraction();
         
     }
 }
