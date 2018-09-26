6d99a6c138be02ff98fee346b1e2437b9bcd8785
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-09-26 13:34:03 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -2,12 +2,25 @@ package week2.task3;
 
 public class Task3 {
 }
-public class Student {
+class Student {
     private String name,id,group,email;
     public String getName(){return name;}   public void setName(String name){this.name=name;}
     public String getId(){return id;}       public void setId(String id){this.id=id;}
     public String getGroup(){return group;}   public void setGroup(String group){this.group=group;}
     public String getEmail(){return email;}   public void setEmail(String email){this.email=email;}
-  
+}
+
+  class laptrinhvien {
+    private String luong,age,sothich;
+    public String getLuong(){return luong;}   public void setLuong(String luong){this.luong=luong;}
+    public String getAge(){return age;}       public void setAge(String age){this.age=age;}
+    public String getSothich(){return sothich;}   public void setSothich(String sothich){this.sothich=sothich;}
+}
+  class hinhchunhat{
+    private String cd,cr,dc;
+    public String getCd(){return cd;}   public void setCd(String cd){this.cd=cd;}
+    public String getCr(){return cr;}       public void setCr(String cr){this.cr=cr;}
+    public String getDc(){return dc;}   public void setDc(String dc){this.dc=dc;}
+  }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
