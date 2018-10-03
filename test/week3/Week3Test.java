bdd16aeda9b549236837bcb5df08388b76a79b63
xuvika3110 <43195650+xuvika3110@users.noreply.github.com>
2018-10-03 11:03:00 +0700

    Update Week3Test.java

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class Week3Test {
     @Test
     // TODO: Viết 5 testcase cho phương thức max()
-    public void max() {
+    public void testmax() {
        Week3 test1 = new Week3();
        assertEquals(8,test1.max(4,8));
        assertEquals(20,test1.max(1,20));
@@ -16,7 +16,7 @@ public class Week3Test {
 
     @Test
     // TODO: Viết 5 testcase cho phương thức minOfArray()
-    public void minOfArray() {
+    public void testminOfArray() {
         Week3 test2 = new Week3();
         int[] array1 = {1,6,8};
         int[] array2 = {6,12};
@@ -32,7 +32,7 @@ public class Week3Test {
 
     @Test
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
-    public void calculateBMI() {
+    public void testcalculateBMI() {
         Week3 test3 = new Week3();
         assertEquals("Thừa cân",test3.calculateBMI(80.00,1.50));
         assertEquals("Bình thường",test3.calculateBMI(60.00,1.72));
