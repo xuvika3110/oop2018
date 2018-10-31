6dc28c31c78a33ec240079d33dc65215b10eea6a
imhson <phamhoangson99@gmail.com>
2018-10-31 13:43:24 +0700

    adddedf

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,32 +1,3 @@
 package week7.task1;
 
-public class Addition extends BinaryExpression{
-    private Expression left;
-    private Expression right;
-
-    public Addition(Expression left, Expression right){
-        this.left = left;
-        this.right = right;
-    }
-
-    @Override
-    public Expression left() {
-        return this.left;
-    }
-
-    @Override
-    public Expression right() {
-        return this.right;
-    }
-
-    @Override
-    public String toString() {
-        return this.left.toString() + " + " + this.right.toString();
-    }
-
-    @Override
-    public int evaluate() {
-        return this.left.evaluate() + this.right.evaluate();
-    }
-
-}
+public class Addition
